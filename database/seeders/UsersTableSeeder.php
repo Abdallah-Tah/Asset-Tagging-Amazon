df69b8550913601b8b26dbd1dbc502b584c7b4dc
Abdallah Mohamed <abdal_cascad@hotmail.com>
2022-07-17 22:46:46 -0400

    added seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Abdallah Mohamed',
+            'email' => 'abdallah@dev.com',
+            'password' => bcrypt('Skull201'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
